3bddcac77eb84f1df70f6982d43997d716f61f25
mariiashybetska <dp190594smo@gmail.com>
2020-02-15 22:58:37 +0200

    setup project

---
+++ check.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+python src/manage.py check
+flake8 src
+python src/manage.py test
+pip check
+
+
---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 's=7$c1s-&8f9*9u^1!s9fif*_^vo3w=8%j@sncl2gthb*+j(tx'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
