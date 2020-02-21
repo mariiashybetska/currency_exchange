645abcc7ce93e3a35b13a3093534b55f80597fe3
mariiashybetska <dp190594smo@gmail.com>
2020-02-21 19:28:37 +0200

    lesson 20: parse rate task

---
+++ src/currency/model_choices.py
@@ -0,0 +1,12 @@
+CURR_USD, CURR_EUR = range(1, 3)
+CURRENCY_CHOICES = (
+    (CURR_USD, 'USD'),
+    (CURR_EUR, 'EUR'),
+)
+
+
+SRC_PB, SRC_MB = range(1, 3)
+SOURCE_CHOICES = (
+    (SRC_PB, 'PrivatBank'),
+    (SRC_MB, 'MonoBank'),
+)
