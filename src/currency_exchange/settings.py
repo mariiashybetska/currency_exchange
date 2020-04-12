d0f0228c473954ca6b09ca023fef0b4822eec9ac
mariiashybetska <dp190594smo@gmail.com>
2020-04-12 17:16:44 +0300

    settings

--- src/currency_exchange/settings.py
+++ src/currency_exchange/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 's=7$c1s-&8f9*9u^1!s9fif*_^vo3w=8%j@sncl2gthb*+j(tx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = False
 
 ALLOWED_HOSTS = []
 
